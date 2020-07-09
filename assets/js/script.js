b1e497f5e183f9ce9bede9053c99a73b3bb20cb7
EmilieVilliere <emilie.vllr@gmail.com>
2020-07-09 17:21:39 +0200

    animation color, back to the top, section scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,67 +1,138 @@
-//  button animation
+//button animation
 
-// function scrollNav() {
-//     jQuery(document).ready(function($) {
-//         $(".items-scroll-to").click(function(event) { // au click sur les items de nav
+function scrollNav() { //fonction d'animation du scroll
+    jQuery(document).ready(function($) {
+        $(".items-scroll-to").click(function(event) { // au click sur les items de nav
         
-//             let sectionNav = $(this).attr("href"); // on recupère la valeur de l'attribut href
+            let sectionNav = $(this).attr("href"); // on recupère la valeur de l'attribut href
 
-//             event.preventDefault(); // on lui fait perdre sa fonctionnalité de href
+            event.preventDefault(); // on lui fait perdre sa fonctionnalité de href
 
-//             let speed = 750; // Durée de l'animation (en ms)
+            let speed = 750; // Durée de l'animation (en ms)
 
-//             $('html, body').animate({ // on anime le html et body
+            $('html, body').animate({ // on anime le html et body
 
-//                     scrollTop: $(sectionNav).offset().top // effectue un scroll vers la section actuelle correspondante
-//                 }, 
+                    scrollTop: $(sectionNav).offset().top // effectue un scroll vers la section actuelle correspondante
+                }, 
                 
-//                 speed ); // Go
+                speed ); // Go
 
-//             return false;
+            return false; 
                 
-//         });
-//     });
-// }
-
- 
-// function animate() {
-
-//     if('input[name=animation]'.is(':checked')) {
-        
-//         scrollNav();
-
-//     } else {
-
-//     alert('tamere');
-
-//     }
-
-// }
-
-// $("#animation").on('click', animate);
-
-//sidebar scroll fuction
-
-jQuery(document).ready(function($) {
-    $(".items-scroll-to").click(function(event) { // au click sur les items de nav
-    
-        let sectionNav = $(this).attr("href"); // on recupère la valeur de l'attribut href
-
-        event.preventDefault(); // on lui fait perdre sa fonctionnalité de href
-
-        let speed = 750; // Durée de l'animation (en ms)
-
-        $('html, body').animate({ // on anime le html et body
-
-                scrollTop: $(sectionNav).offset().top // effectue un scroll vers la section actuelle correspondante
-            }, 
-            
-            speed ); // Go
-
-        return false;
-            
+        });
     });
-});
+}
+
+
+function animate() { // fonction d'animation
+
+  $("#animation").on('change', function(){
+
+    if($(this).is(':checked')) { // si le checkbox est checked 
+        
+      scrollNav(); // alors tu m'execute la fonction 
+
+    } else {
+
+      return false;
+
+    }
+
+  })
+
+}
+
+animate();
+
+
+//section scroll section 
+
+// $(document).ready(function() {
+
+//     $(function(){
+
+//       $(window).scroll(function(e) {
+
+//         let position = window.scrollY; // recupère la position de l'axe Y du scroll
+
+//         let section2 =  $('#section-2').offset(); // recupère les position Top de l'élement
+//         let posTopSec2 = section2.top;
+
+//         let section3 =  $('#section-3').offset();
+//         let posTopSec3 = section3.top;
+
+//         let section4 =  $('#section-4').offset();
+//         let posTopSec4 = section4.top;
+
+//         if(position > 0 && position < posTopSec2) { // si l'élement est compris entre 0 et position Top de la seconde section
+
+//           $("#sec1").addClass("active");  // ajoute class active
+//           $("#sec1").css({"background-color": "white", "color": "#a43f49"}); // ajout de style css
+          
+//         } 
+        
+//         if (position >= posTopSec2 && position < posTopSec3) { // si l'élement est compris entre position Top de la seconde section et celle de la section 3
+
+//           $("#sec1").removeClass("active"); // on retire la class active de l'élement précédent 
+//           $("#sec1").css({"background-color": "#a43f49", "color": "white"}); // ainsi que les styles css 
+        
+//           $("#sec2").addClass("active"); 
+//           $("#sec2").css({"background-color": "white", "color": " #a43f49"});  // pour le mettre à l'élément actuel 
+
+//         } 
+        
+//         if (position >= posTopSec3 && position < posTopSec4) { // ainsi de suite 
+
+//           $("#sec2").removeClass("active");
+//           $("#sec2").css({"background-color": "#a43f49", "color": "white"}); 
+
+//           $("#sec3").addClass("active");  
+//           $("#sec3").css({"background-color": "white", "color": " #a43f49"}); 
+
+//         } 
+
+//         if(position > posTopSec4) {
+
+//           $("#sec3").removeClass("active");
+//           $("#sec3").css({"background-color": "#a43f49", "color": "white"}); 
+
+//           $("#sec4").addClass("active");  
+//           $("#sec4").css({"background-color": "white", "color": " #a43f49"}); 
+
+//         }
+
+//       });
+//   });
+  
+// });
+
+// section scroll section CORRECTION  
+
+function section() {
+  $(".section").each(function() {
+
+    let $this = $(this); // on recupère une liste d'éléments actuels sous forme d'objet 
+
+    let topEdge = $this.offset().top - 80; // on recupère la position Top - le padding 80px
+    let bottomEdge = topEdge + $this.height(); // on recupère la position Bottom en soustrayant la dimension du contenu - la valeur top
+    let windowScroll = $(window).scrollTop(); // on initie une variable qui contient le mouvement du scroll de l'écran
+
+    if(topEdge < windowScroll && bottomEdge > windowScroll) { // le scroll et compris entre la valeur la + grand et celle + petite 
+
+      let section = $this.data("section"); // on récupère la section correspondante
+
+      let filtre = "[href=#" + section + "]"; // on recupère le href correspondant
+      let lien = $("a").filter(filtre); // on utilise la fonction filter pour faire correspondre l'id et le href
+      lien.closest("li").addClass("active").siblings().removeClass("active"); // active contient le css
+      // enfin si les deux match, on applique la class active et removeClass active à tous les autres.
+
+    }
+
+  });
+
+}
+
+section();
 
 
 // section 3 layout
@@ -101,3 +172,65 @@ $(".isotope-wrapper").each(function() {
     filter();
 
   });
+
+
+// button back to the top 
+
+function backtop(){
+
+  $(window).scroll(function(e) {
+
+    let arrow = $('#arrow');
+    let position = window.scrollY;
+
+    let section3 =  $('#section-3').offset();
+    let posTopSec3 = section3.top - 180; 
+
+    if(position >= posTopSec3) {
+
+      arrow.css('display','block');
+
+    }
+
+    if(position <= 10) {
+
+      arrow.css('display','none');
+
+    }
+
+  });
+
+}
+
+backtop();
+
+$("#arrow").on('click', function() {
+
+  $("html, body").animate({scrollTop: 0}, 1000);
+
+  return false;
+
+})
+
+
+// palet color change
+
+function color() {
+
+  let palet = $('#palet');
+  let color = palet.val();
+
+  let elements = [$('#menu'), $('.white-button a'), $('.line')];
+
+  $(palet).on('change', function() {
+
+    for(let item of elements) {
+
+      item.css('background-color', color);
+    }
+
+  });
+
+}
+
+color();
